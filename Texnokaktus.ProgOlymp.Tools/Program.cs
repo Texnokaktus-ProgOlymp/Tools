9555844927cef00392578a4ac2abc702ff4086a7
Artem Krylov <kav128@outlook.com>
2025-02-17 23:08:46 +0300

    tools-service

--- Texnokaktus.ProgOlymp.Tools/Program.cs
+++ Texnokaktus.ProgOlymp.Tools/Program.cs
@@ -1,6 +1,5 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Serilog;
-using Texnokaktus.ProgOlymp.Tools.Extensions;
 using Texnokaktus.ProgOlymp.Tools.Services;
 using Texnokaktus.ProgOlymp.Tools.Services.Abstractions;
 
@@ -14,16 +13,18 @@ builder.Services.AddOpenApi();
 
 builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
-builder.Services
-       .AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
-        })
-       .AddConfiguredJwtBearer(builder.Configuration);
+// builder.Services
+//        .AddAuthentication(options =>
+//         {
+//             options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//             options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//             options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+//         })
+//        .AddConfiguredJwtBearer(builder.Configuration);
 
-builder.Services.AddAuthorization();
+// builder.Services.AddAuthorization();
+
+builder.Services.AddSingleton(TimeProvider.System);
 
 var app = builder.Build();
 
@@ -33,13 +34,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options => options.ConfigObject.Urls = [new() { Name = "v1", Url = "/openapi/v1.json" }]);
 }
 
-app.UseAuthentication();
-app.UseAuthorization();
+// app.UseAuthentication();
+// app.UseAuthorization();
 
-app.MapPost("wipe", async (IEnumerable<IToolsService> services) =>
+app.MapPost("wipe", async Task<Results<NoContent, ForbidHttpResult>>(TimeProvider timeProvider, IEnumerable<IToolsService> services) =>
 {
+    if (timeProvider.GetUtcNow() >= new DateTimeOffset(2025, 02, 20, 00, 00, 00, TimeSpan.FromHours(3)))
+        return TypedResults.Forbid();
+
     foreach (var service in services)
         await service.ExecuteWipeAsync();
+
+    return TypedResults.NoContent();
 });
 
 await app.RunAsync();
